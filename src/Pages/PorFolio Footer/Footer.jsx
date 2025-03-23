deb80a48b75050afd6f280edadaf214c69c3209a
debugmanoj <manojkumarwork01@gmail.com>
2025-03-23 15:58:46 +0530

    Added animation Images And Footer

--- src/Pages/PorFolio Footer/Footer.jsx
+++ src/Pages/PorFolio Footer/Footer.jsx
@@ -3,12 +3,50 @@ import React from 'react'
 const Footer = () => {
   return (
     <div id="Footer-Section" className="container mx-auto px-1 md:px-0 lg:px-28 py-4 font-figtree w-full">
-        <div className="font-bold text-3xl md:text-5xl lg:text-5xl text-[#04263e]">
-            About
+      <div className="font-bold text-3xl md:text-5xl lg:text-5xl text-[#04263e]">
+        About
+      </div>
+      <div className="mt-[1.5%] lg:grid grid-cols-2 gap-5">
+        <div className="text-gray-500 text-sm md:text-base lg:text-base lg:w-full mt-[0.8%]">
+          I’m Manoj Kumar, a passionate MERN Stack Developer from Vellore, Tamil Nadu, India, with a strong enthusiasm for working with tech startups. I believe that learning is a lifelong journey, and I embrace knowledge from everyone, regardless of their background.
+          <div className='mt-[3%]'>
+            <span className='text-black'>Little Background</span>
+
+            <span className="text-gray-500 text-sm md:text-base lg:text-base lg:w-full mt-[0.2%] block">
+              I completed my SSLC with 86% and HSC with 84%, followed by a Bachelor’s degree in Computer Science Engineering. After graduation, I discovered my passion for full-stack web development and chose the MERN stack as my specialization. Since then, I have been dedicating my time to mastering my skills, building scalable applications, and staying up-to-date with the latest industry trends.
+
+              Currently, I’m working as a Software Developer at Wonder Worth Solutions, where I contribute to building robust and efficient applications while continuously expanding my expertise.........
+
+            </span>
+          </div>
         </div>
-        <div className="text-gray-500 text-sm md:text-base lg:text-base lg:w-[60%] mt-[0.8%]">
-            Need To Work
+        <div id="default-carousel" className=" relative w-full">
+          <div className="overflow-hidden rounded-lg md:h-96 relative">
+            <div className="absolute w-full h-full">
+              <img className="absolute w-full h-full object-cover opacity-0 animate-fade1" src="/Manoj_1.webp" alt="Carousel1" />
+              <img className="absolute w-full h-full object-cover opacity-0 animate-fade2" src="/Nature.webp" alt="Carousel2" />
+            </div>
+          </div>
         </div>
+        <div>
+          <a href="" className='p-4 py-2 bg-[#0370DD] text-white rounded-lg font-bold transition-all duration-500 hover:bg-blue-950'>Read More</a>
+        </div>
+        <div>
+        </div>
+      </div>
+      <div className="w-full mt-6">
+        <div >
+          <div  className="flex justify-between items-center transition-all duration-500  border-gray-200 px-1 py-3">
+            <div className='flex gap-7'>
+
+            <a href="https://www.linkedin.com/in/manoj-kumar10" target="_blank" className=" text-xs md:text-xs lg:text-xs font-medium  text-[#04263e] "> Linked In </a>
+            <a href="https://github.com/debugmanoj" target="_blank" className=" text-xs md:text-xs lg:text-xs font-medium  text-[#04263e] "> GitHub </a>
+            </div>
+            <span className={`text-black font-medium text-sm md:text-base lg:text-base cursor-default  md:inline`}>Working Fulltime</span>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
       fontFamily: {
         figtree: ["Figtree", "sans-serif"],
       },
+      keyframes: {
+        fade1: {
+          "0%, 50%": { opacity: "1" },
+          "51%, 100%": { opacity: "0" },
+        },
+        fade2: {
+          "0%, 50%": { opacity: "0" },
+          "51%, 100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fade1: "fade1 10s infinite",
+        fade2: "fade2 10s infinite",
+      },
     },
   },
   plugins: [],
